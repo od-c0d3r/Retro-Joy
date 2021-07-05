1abcd9f7e824d21245beb0241ab1669478308b85
od-c0d3r <omar_rashad@aol.com>
2021-07-05 16:39:10 +0200

    Setup: Datebase seeds with Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.create!([{
+    name: "Omar",
+    username: "omarrashad",
+},
+{
+    name: "Bella",
+    username: "bellaswan",
+},
+{
+    name: "Gotem",
+    username: "gotemall",
+}])
+
+p "Created #{User.count} users"
