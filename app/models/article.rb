c4d10620be4d6f8097e63d36770e1bc6eff8e267
od-c0d3r <omar_rashad@aol.com>
2021-07-06 16:23:25 +0200

    Datebase: Articles-Votes Associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,7 @@ class Article < ApplicationRecord
     has_one_attached :image
 
     belongs_to :author, class_name: 'User'
-
+    
+    has_many :votes
+    has_many :liked_users, through: 'votes'
 end
