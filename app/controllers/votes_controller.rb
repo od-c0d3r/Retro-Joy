ed8f1cc694cdb329b1451a08b177312cbe8fff4e
od-c0d3r <omar_rashad@aol.com>
2021-07-06 16:47:51 +0200

    Setup: Create Votes Controller

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,17 @@
+class VotesController < ApplicationController
+  
+  def create
+    vote = Vote.new(vote_params)
+    if vote.save
+      redirect_to root_path
+    else 
+      flash[:danger] = "Something went wrong!"
+    end
+  end
+
+  private
+
+  def vote_params
+     params.require(:vote).permit(:user_id, :article_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  root 'application#index'
+  post   'votes/create'
+  delete 'votes/destroy'
+
+  get    'articles/new'
+  get    'articles/create'
+  get    'articles/edit'
+  get    'articles/update'
+
+  get    'users/new'
+  get    'users/create'
+  get    'users/edit'
+  get    'users/update'
+
+  root   'application#index'
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
