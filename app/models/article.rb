0d1c1a44752b7772dee716d3bf7fcf6d4eb51f45
od-c0d3r <omar_rashad@aol.com>
2021-07-06 16:57:41 +0200

    Datebase: Articles-Categories Associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ApplicationRecord
+    validates :catergory_id,  presence: true
+
     has_one_attached :image
 
     belongs_to :author, class_name: 'User'
     
     has_many :votes
     has_many :liked_users, through: 'votes'
+
+    belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_140234) do
+ActiveRecord::Schema.define(version: 2021_07_06_145357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_07_06_140234) do
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
