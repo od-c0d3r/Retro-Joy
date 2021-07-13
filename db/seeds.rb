0ceb3a5df1f970e5b0fb9baa00767d8d1a537392
od-c0d3r <omar_rashad@aol.com>
2021-07-13 11:57:10 +0200

    Deploy: Remove turncat funtion from SeedS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 Category.destroy_all
 ActiveRecord::Base.connection.execute("TRUNCATE #{Category.table_name} RESTART IDENTITY")
-User.destroy_all
-ActiveRecord::Base.connection.execute("TRUNCATE #{User.table_name} RESTART IDENTITY")
 
 Category.create!([
     {name: "Business", priority: nil},
