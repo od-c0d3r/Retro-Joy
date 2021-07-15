64c5027a1a2b13bb627e8d574d0cf4cf8d6d9c31
od-c0d3r <omar_rashad@aol.com>
2021-07-15 15:47:34 +0200

    Test: Article associations and validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -18,5 +18,26 @@ RSpec.describe Article, type: :model do
       t = Article.reflect_on_association(:author)
       expect(t.macro).to eq(:belongs_to)
     end
+
+    it "should have many votes" do
+      t = Article.reflect_on_association(:votes)
+      expect(t.macro).to eq(:has_many)
+    end
+
+    it "should belongs to a category" do
+      t = Article.reflect_on_association(:category)
+      expect(t.macro).to eq(:belongs_to)
+    end
+
+    it 'validates presence of title' do
+      Category.create!(name: 'Sports')
+      record = Article.new(author_id: user.id, title: 'usertest', text: 'test@test.com', category_id: Category.first.id)
+      record.title = ''
+      record.validate
+      expect(record.errors[:title]).to include("can't be blank") 
+      record.title = 'Title exists'
+      record.validate
+      expect(record.errors[:title]).to_not include("can't be blank") 
+    end
   end
 end
